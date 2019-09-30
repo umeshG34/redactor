bcfb43ad33e05d1b3881f0fe36e7d217a990949a
Umesh Sai Gurram <42475265+umeshG34@users.noreply.github.com>
2019-09-30 11:46:46 -0400

    Update setup.py

--- redactor_project/setup.py
+++ redactor_project/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='redactor',
     version='1.0',
-    author='You Name',
-    authour_email='your ou email',
+    author='Umesh Sai Gurram',
+    authour_email='umXsXsXiXX@XX.com',
     packages=find_packages(exclude=('tests', 'docs')),
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
